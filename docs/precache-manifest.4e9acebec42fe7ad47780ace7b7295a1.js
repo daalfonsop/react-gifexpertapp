9f314bc12d8718bdba205245c2119cc36d70d5f7
Diego Alfonso <diegoandresalfonso@hotmail.com>
2020-10-19 14:31:14 -0500

    docs agregado

---
+++ docs/precache-manifest.4e9acebec42fe7ad47780ace7b7295a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef133843106c470383923a16a79aaebe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60a1869e37aca3cbc408",
+    "url": "/static/css/main.bd2a622b.chunk.css"
+  },
+  {
+    "revision": "18772ccb525ea2d14384",
+    "url": "/static/js/2.57075067.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.57075067.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60a1869e37aca3cbc408",
+    "url": "/static/js/main.0530d4a6.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
